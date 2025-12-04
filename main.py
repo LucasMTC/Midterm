0277a94f3ea76116bdd665d72dfaf6fdb4c42bea
LucasMTC <lucas.callisperis@gmail.com>
2025-12-03 23:12:24 -0600

    Added original Dijkstra NeetCode Version

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from midterm!")
+import heapq
 
+def originalDijkstra(edges, n, src):
+    adj = {}
+    for i in range(1, n + 1):
+        adj[i] = []
+
+    for s, d, w in edges:
+        adj[s].append([d, w])
+
+    shortest = {}
+    minHeap = [[0, src]]
+    while minHeap:
+        w1, n1 = heapq.heappop(minHeap)
+        if n1 in shortest:
+            continue
+        shortest[n1] = w1
+
+        for n2, w2 in adj[n1]:
+            if n2 not in shortest:
+                heapq.heappush(minHeap, [w1 + w2, n2])
+    return shortest
+
+
+def main():
+    with open("input.txt") as input:
+        graph = []
+        for line in input:
+            node = line.split()
+            for i in range(len(node)):
+                if node[i].isnumeric():
+                    node[i] = int(node[i])
+            graph.append(node)
 
 if __name__ == "__main__":
     main()
